b216994c1b611d0f0663ab04d217dad4aa10d109
Tom Lorius <tomlorius.mail@gmail.com>
2023-11-24 12:38:23 +0100

    Bonus iteration finished - no jasmine errors

--- src/viking.js
+++ src/viking.js
@@ -47,9 +47,43 @@ class Saxon extends Soldier{
 }
 
 // War
+
 class War {
     constructor(){
         this.vikingArmy = [];
         this.saxonArmy = [];
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack(){
+        let rdmSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        let rdmViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let result = this.saxonArmy[rdmSaxon].receiveDamage(this.vikingArmy[rdmViking].strength);
+        if (this.saxonArmy[rdmSaxon].health <= 0){
+            this.saxonArmy.splice(rdmSaxon, 1);
+        }
+        return result;
+    }
+    saxonAttack(){
+        let rdmSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        let rdmViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let result = this.vikingArmy[rdmViking].receiveDamage(this.saxonArmy[rdmSaxon].strength);
+        if (this.vikingArmy[rdmViking].health <= 0){
+            this.vikingArmy.splice(rdmViking, 1);
+        }
+        return result;
+    }
+    showStatus(){
+        if (this.saxonArmy.length === 0){
+            return `Vikings have won the war of the century!`
+        } else if (this.vikingArmy.length === 0) {
+            return `Saxons have fought for their lives and survived another day...`
+        } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+            return `Vikings and Saxons are still in the thick of battle.`
+        }
     }
 }
